6916d203e8d213a7f102b3c7d825c0df469b2fe8
Ivan Varga <ivan.varga@five.agency>
2020-03-24 11:28:49 +0100

    Refactoring

--- app/src/main/java/com/example/sample/Histogram.kt
+++ app/src/main/java/com/example/sample/Histogram.kt
@@ -7,6 +7,7 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.constraintlayout.widget.ConstraintSet.*
 
 private const val DEFAULT_COLUMN_WIDTH = 100
 private const val DEFAULT_MAX_COLUMN_VALUE_OFFSET_TOP = 20
@@ -46,7 +47,7 @@ class Histogram : ConstraintLayout {
     }
 
     /**
-     * We subtract the top from [getAvailableHeight] because small values have a large value for top (because they need to be shorter).
+     * We subtract the top from [getAvailableHeight] because small values have a large value for top (they need to be shorter).
      */
     fun sortColumns(descending: Boolean = false) {
         if (descending) columnList.sortByDescending { getAvailableHeight() - it.second.top } else columnList.sortBy { getAvailableHeight() - it.second.top }
@@ -58,8 +59,7 @@ class Histogram : ConstraintLayout {
         dataSet.forEach {
             val top = getColumnTop(it.second)
 
-            val column = getColumn(index)
-            column.apply {
+            val column = getColumn(index).apply {
                 setColumnColor(columnColor)
                 id = View.generateViewId()
                 setColumnTop(top)
@@ -79,23 +79,25 @@ class Histogram : ConstraintLayout {
         set.clone(this)
 
         var previousColumn = columnList.firstOrNull()?.second ?: return
-        set.connect(previousColumn.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 0)
-        set.connect(previousColumn.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0)
-        set.connect(previousColumn.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0)
+
+        //startToStartOf=parent, bottomToBottomOf=parent
+        set.connect(previousColumn.id, START, PARENT_ID, START, 0)
+        set.connect(previousColumn.id, BOTTOM, PARENT_ID, BOTTOM, 0)
 
         for (i in 1 until columnList.size) {
             val currentColumn = columnList[i].second
-            set.connect(currentColumn.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0)
-            set.connect(currentColumn.id, ConstraintSet.START, previousColumn.id, ConstraintSet.END, columnSpacing)
-            set.connect(currentColumn.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0)
+
+            //startToEndOf=previousColumn, bottomToBottomOf=parent
+            set.connect(currentColumn.id, START, previousColumn.id, END, columnSpacing)
+            set.connect(currentColumn.id, BOTTOM, PARENT_ID, BOTTOM, 0)
 
             previousColumn = currentColumn
         }
 
-        animateColumnReordering(set)
+        applyWithAnimation(set)
     }
 
-    private fun animateColumnReordering(endConstraintSet: ConstraintSet) {
+    private fun applyWithAnimation(endConstraintSet: ConstraintSet) {
         TransitionManager.beginDelayedTransition(this)
         endConstraintSet.applyTo(this)
     }
